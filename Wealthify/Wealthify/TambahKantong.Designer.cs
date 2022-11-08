175b69e6e7e63ce8790aa316b08c7faec4881479
evanjat2 <ann.jati2@gmail.com>
2022-11-09 01:59:47 +0700

    fix issue _jenis_kantong doesn't exist

--- Wealthify/Wealthify/TambahKantong.Designer.cs
+++ Wealthify/Wealthify/TambahKantong.Designer.cs
@@ -42,9 +42,10 @@
             // 
             this.lblJenisKantong.AutoSize = true;
             this.lblJenisKantong.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblJenisKantong.Location = new System.Drawing.Point(45, 74);
+            this.lblJenisKantong.Location = new System.Drawing.Point(64, 123);
+            this.lblJenisKantong.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblJenisKantong.Name = "lblJenisKantong";
-            this.lblJenisKantong.Size = new System.Drawing.Size(111, 19);
+            this.lblJenisKantong.Size = new System.Drawing.Size(165, 29);
             this.lblJenisKantong.TabIndex = 0;
             this.lblJenisKantong.Text = "Jenis Kantong";
             // 
@@ -52,9 +53,10 @@
             // 
             this.lblNamaKantong.AutoSize = true;
             this.lblNamaKantong.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblNamaKantong.Location = new System.Drawing.Point(45, 113);
+            this.lblNamaKantong.Location = new System.Drawing.Point(64, 188);
+            this.lblNamaKantong.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNamaKantong.Name = "lblNamaKantong";
-            this.lblNamaKantong.Size = new System.Drawing.Size(116, 19);
+            this.lblNamaKantong.Size = new System.Drawing.Size(172, 29);
             this.lblNamaKantong.TabIndex = 1;
             this.lblNamaKantong.Text = "Nama Kantong";
             // 
@@ -62,9 +64,10 @@
             // 
             this.lblSaldo.AutoSize = true;
             this.lblSaldo.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblSaldo.Location = new System.Drawing.Point(45, 154);
+            this.lblSaldo.Location = new System.Drawing.Point(64, 257);
+            this.lblSaldo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSaldo.Name = "lblSaldo";
-            this.lblSaldo.Size = new System.Drawing.Size(50, 19);
+            this.lblSaldo.Size = new System.Drawing.Size(74, 29);
             this.lblSaldo.TabIndex = 2;
             this.lblSaldo.Text = "Saldo";
             // 
@@ -75,44 +78,49 @@
             "Tunai",
             "E-Wallet",
             "Rekening"});
-            this.cbJenisKantong.Location = new System.Drawing.Point(183, 74);
+            this.cbJenisKantong.Location = new System.Drawing.Point(261, 119);
+            this.cbJenisKantong.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cbJenisKantong.Name = "cbJenisKantong";
-            this.cbJenisKantong.Size = new System.Drawing.Size(154, 23);
+            this.cbJenisKantong.Size = new System.Drawing.Size(218, 33);
             this.cbJenisKantong.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(107, 27);
+            this.label1.Location = new System.Drawing.Point(153, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(171, 25);
+            this.label1.Size = new System.Drawing.Size(273, 38);
             this.label1.TabIndex = 4;
             this.label1.Text = "Tambah Kantong";
             // 
             // tbNamaKantong
             // 
-            this.tbNamaKantong.Location = new System.Drawing.Point(183, 113);
+            this.tbNamaKantong.Location = new System.Drawing.Point(261, 188);
+            this.tbNamaKantong.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbNamaKantong.Name = "tbNamaKantong";
             this.tbNamaKantong.PlaceholderText = "Masukkan nama kantong";
-            this.tbNamaKantong.Size = new System.Drawing.Size(154, 23);
+            this.tbNamaKantong.Size = new System.Drawing.Size(218, 31);
             this.tbNamaKantong.TabIndex = 5;
             // 
             // tbSaldo
             // 
-            this.tbSaldo.Location = new System.Drawing.Point(183, 150);
+            this.tbSaldo.Location = new System.Drawing.Point(261, 250);
+            this.tbSaldo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbSaldo.Name = "tbSaldo";
             this.tbSaldo.PlaceholderText = "Masukkan saldo awal";
-            this.tbSaldo.Size = new System.Drawing.Size(154, 23);
+            this.tbSaldo.Size = new System.Drawing.Size(218, 31);
             this.tbSaldo.TabIndex = 6;
             // 
             // btnSimpan
             // 
             this.btnSimpan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(196)))), ((int)(((byte)(68)))));
             this.btnSimpan.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnSimpan.Location = new System.Drawing.Point(144, 205);
+            this.btnSimpan.Location = new System.Drawing.Point(206, 342);
+            this.btnSimpan.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSimpan.Name = "btnSimpan";
-            this.btnSimpan.Size = new System.Drawing.Size(89, 30);
+            this.btnSimpan.Size = new System.Drawing.Size(127, 50);
             this.btnSimpan.TabIndex = 7;
             this.btnSimpan.Text = "Simpan";
             this.btnSimpan.UseVisualStyleBackColor = false;
@@ -120,10 +128,10 @@
             // 
             // TambahKantong
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(387, 264);
+            this.ClientSize = new System.Drawing.Size(553, 440);
             this.Controls.Add(this.btnSimpan);
             this.Controls.Add(this.tbSaldo);
             this.Controls.Add(this.tbNamaKantong);
@@ -133,6 +141,7 @@
             this.Controls.Add(this.lblNamaKantong);
             this.Controls.Add(this.lblJenisKantong);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "TambahKantong";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "TambahKantong";
