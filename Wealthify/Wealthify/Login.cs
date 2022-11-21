03570edc0296de5569b94223a5733cebe9367165
evanjat2 <ann.jati2@gmail.com>
2022-11-22 00:47:32 +0700

    finishing login

--- Wealthify/Wealthify/Login.cs
+++ Wealthify/Wealthify/Login.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace Wealthify
 {
@@ -22,16 +24,54 @@ namespace Wealthify
 
         }
 
+        private void addParameters()
+        {
+            CRUD.cmd.Parameters.Clear();
+            CRUD.cmd.Parameters.AddWithValue("email", tbEmail.Text.Trim());
+            CRUD.cmd.Parameters.AddWithValue("password", tbPassword.Text.Trim());
+        }
+
         private void linkForgot_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
 
         }
 
+        private void checkValidation(string mySQL)
+        {
+            CRUD.cmd = new NpgsqlCommand(mySQL, CRUD.con);
+            addParameters();
+            var dt = CRUD.PerformCrud(CRUD.cmd);
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Berhasil Login", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Laporan flaporan = new Laporan();
+                flaporan.Show();
+                this.Hide();
+
+            }
+            else
+            {
+                MessageBox.Show("Email atau password salah", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            Laporan flaporan = new Laporan();
-            flaporan.Show();
-            this.Hide();
+            if (string.IsNullOrEmpty(tbEmail.Text.Trim()) || string.IsNullOrEmpty(tbPassword.Text.Trim()))
+            {
+                MessageBox.Show("Masukkan email dan password", "Insert Data", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            try
+            {
+                CRUD.sql = "SELECT * FROM pengguna WHERE email = @email AND password = @password";
+                checkValidation(CRUD.sql);
+                return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured: " + ex.Message, "Gagal Register", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void label3_Click(object sender, EventArgs e)
